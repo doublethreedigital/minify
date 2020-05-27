fb9c858ca8166fdc37fa13a1bf20ac822545d026
Duncan McClean <duncan@mcclean.co.uk>
2020-05-27 17:51:22 +0100

    Added a clear cache command - the generation has now broke too

---
+++ src/Console/Commands/ClearMinifyCache.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DoubleThreeDigital\Minify\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+use SplFileInfo;
+use Statamic\Console\RunsInPlease;
+
+class ClearMinifyCache extends Command
+{
+    use RunsInPlease;
+
+    protected $signature = 'minify:clear';
+    protected $description = 'Clear your Minify cache.';
+
+    public function handle()
+    {
+        $this->info('Clearing your Minify cache..');
+        collect(File::allFiles(config('filesystems.disks.public.root').'/_minify/'))
+            ->each(function (SplFileInfo $file) {
+                $this->line("Clearing {$this->getFilename()}");
+                File::delete($file);
+                Cache::forget($file->getFilename());
+            });
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,11 +2,16 @@
 
 namespace DoubleThreeDigital\Minify;
 
+use DoubleThreeDigital\Minify\Console\Commands\ClearMinifyCache;
 use DoubleThreeDigital\Minify\Tags\MinifyTag;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
+    protected $commands = [
+        ClearMinifyCache::class,
+    ];
+
     protected $tags = [
         MinifyTag::class,
     ];
